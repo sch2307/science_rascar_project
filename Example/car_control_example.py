31448f87ab2365063a78eaf830b8370799510226
ChoiJuwon <sch2307@naver.com>
2021-10-30 13:41:57 +0900

    [UPDATE] Fix Ultrasonic GPIO Pin

--- Example/car_control_example.py
+++ Example/car_control_example.py
@@ -8,24 +8,32 @@ if __name__ == '__main__':
     try:
         # Example Of Front Servo Motor Control
         steering = front_wheels.Front_Wheels(db='config')
-        steering.center_alignment()
-        time.sleep(1)
-        
+        steering.ready() #PWM 셋업
+
+        steering.center_alignment() #핸들 중앙정렬
+        time.sleep(1) #1초 스레드 슬립
+
+        steering.turn_left() #왼쪽 조향
+        steering.turn_right() #오른쪽 조향
+
+        steering.turn(-15) #왼쪽 조향 / 앵글값(최대 조향각 35도)
+        steering.turn(15) #오른쪽 조향 / 앵글값(최대 조향각 35도)
+
         # Example Of Real Motor Control
         accelerator = rear_wheels.Rear_Wheels(db='config')
-        accelerator.ready()
-        accelerator.go_forward(50)
+        accelerator.ready() #PWM 셋업
+        accelerator.go_forward(50) # 전진(50/100의 속도로)
         time.sleep(1)
-        accelerator.stop()
+        accelerator.stop() #모터 정지
         time.sleep(1)
-        accelerator.go_backward(50)
+        accelerator.go_backward(50) #후진(50/100의 속도로)
         time.sleep(1)
 
         accelerator.stop()
         accelerator.power_down()
         
         # Example of Ultrasonic Sensor
-        distance_detector = Supersonic_Sensor.Supersonic_Sensor(5)
+        distance_detector = Supersonic_Sensor.Supersonic_Sensor(20)
 
         for i in range(10):
             distance = distance_detector.get_distance()
